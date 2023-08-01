e7b1cbebb865167cd6df80ad9644e20c12785468
Dr.Engel <angelantonio01111@gmail.com>
2023-08-01 12:04:12 -0600

    primer commit

---
+++ src/dia1/Dia1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dia1;
+import java.util.Scanner;
+        
+/**
+ *
+ * @author CesarLopez
+ */
+public class Dia1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner tem = new Scanner(System.in);
+        String nomb;
+        int edad;
+        System.out.println("Ingrese nombre");
+        nomb = tem.nextLine();
+        System.out.println("Ingrese edad");
+        edad = tem.nextInt();
+    }
+    
+}
